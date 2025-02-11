c48f95d22514e3cd198887c20170837bc87803e8
Catalin Cechin <154013106+ZEEEEER0@users.noreply.github.com>
2025-02-11 11:06:53 +0200

    Add button

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+#define BUTTON_PIN 2
+#define DEBOUCE_DELAY 150 // ms
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() 
+{
+  static uint32_t nextCheckTime = 0;
+  
+  if (digitalRead(BUTTON_PIN) == LOW)
+  {
+    if (millis() >= nextCheckTime)
+    {
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    }
+    nextCheckTime = millis() + DEBOUCE_DELAY;
+    
+  }
+}
