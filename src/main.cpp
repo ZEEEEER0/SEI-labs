88bd65b169eb1f2a0450361550b1174988582ee1
Catalin Cechin <154013106+ZEEEEER0@users.noreply.github.com>
2025-02-11 16:16:06 +0200

    Add LCD

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,35 @@
 void setup(void)
 {
   own_stdio_setup();
-  Serial.println("System started\n");
+  Serial.println("System started\n\r");
+  pinMode(LED_BUILTIN, OUTPUT);
 }  
+
+void LedControl(const char *cmd)
+{
+  if (strcmp(cmd, "on") == 0)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("LED is on\n\r");
+  }
+  else if (strcmp(cmd, "off") == 0)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("LED is off\n\r");
+  }
+}
 
 void loop(void)
 {
   // Your code here
+}
+
+void serialEvent(void)
+{
+  char cmd[10];
+  printf("Enter command: ");
+  scanf("%10s", cmd);
+
+  printf("%s:\n", cmd);
+  LedControl(cmd);
 }
