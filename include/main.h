e84dfddcfd8f2b3bc9140987f98af5dac4a24136
Catalin Cechin <154013106+ZEEEEER0@users.noreply.github.com>
2025-03-10 19:47:45 +0200

    Status

--- include/main.h
+++ include/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include "own_stdio.h"
+
 // Include necessary libraries
 #include <Arduino.h>
 #include <config.h>
+#include <stdio.h>
+#include <string.h>
 
-
-
+void LedControl(const char *cmd);
 #endif // MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,30 @@
 void setup(void)
 {
   own_stdio_setup();
-  Serial.println("System started\n");
+  Serial.println("System started\n\r");
+  pinMode(LED_BUILTIN, OUTPUT);
 }  
 
-void loop(void)
+void LedControl(const char *cmd)
 {
-  // Your code here
+  if (strcmp(cmd, "on") == 0)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("LED is on\n\r");
+  }
+  else if (strcmp(cmd, "off") == 0)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("LED is off\n\r");
+  }
+}
+
+void serialEvent(void)
+{
+  char cmd[10];
+  printf("Enter command: ");
+  scanf("%10s", cmd);
+
+  printf("%s:\n", cmd);
+  LedControl(cmd);
 }
