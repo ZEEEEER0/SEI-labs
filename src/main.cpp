0e821a083c51c7e9aa2d6000045a5deac4bdb8a9
Catalin Cechin <154013106+ZEEEEER0@users.noreply.github.com>
2025-03-25 15:36:07 +0200

    save

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
-#include "main.h"
+#include <main.h>
 
-void setup()
-{
-    buttonLedTaskSetup();
-    setBlinkFrequencyTaskSetup();
-    idleTaskSetup();
 
-    // Alte inițializări necesare
-    Serial.begin(BAUDRATE);
-    printf("Setup completed\n");
-}
+void loop(void) 
+   
 
-void loop()
-{
-
-}
